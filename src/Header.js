f73b4b87398c0b52628f0952a0ee9d31e9abadc4
KLan Stovall <kland13@gmail.com>
2021-11-01 20:17:43 -0700

    Feature 2 complete

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>City Explorer</h1>
+            </div>
+        )
+    }
+}
