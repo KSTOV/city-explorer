55beffdfe44c872da851716c5d38d1d83f5003f7
KLan Stovall <kland13@gmail.com>
2021-11-02 00:49:05 -0700

    lab 06 complete

--- README.md
+++ README.md
@@ -6,30 +6,53 @@
 ![Web Cycle](WebCycle.jpg)
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+City Explorer displays a location on a map based on the user input
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+Visit: [City Explorer](https://ks-cityexplorer.netlify.app/)
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+HTML, CSS , JavaScript, React, LocationIQ API
 
 ## Change Log
+
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
 
 01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
 
 ## Credit and Collaborations
-<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+LocationIQ API
 
 ## Features
 
-Name of feature: ________________________________
+Name of feature: Locations
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 1 hour
 
-Start time: _____
+Start time: 1:30pm
+Finish time: 2:00pm
 
-Finish time: _____
+Actual time needed to complete: 30 minutes
+________________________________________________________________
+Name of feature: Map
 
-Actual time needed to complete: _____
+Estimate of time needed to complete: 2 hours
+
+Start time: 4:30pm
+Finish time: 7:00pm
+
+Actual time needed to complete: 2 hours 30 minutes
+________________________________________________________________
+Name of feature: Errors
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 10:00pm
+Finish time: 12:30am
+
+Actual time needed to complete: 2 hours 30 minutes
+________________________________________________________________
--- src/CityData.js
+++ src/CityData.js
@@ -3,54 +3,71 @@ import axios from "axios";
 import Form from "react-bootstrap/Form";
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import Error from './Error.js';
 
 export default class CityData extends Component {
     constructor(props) {
         super(props)
         this.state = {
           cityValue: '',
+          error: false,
+          setError: ''
         }
     }
     
     handleClick = async () => {
-        const usUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityValue}&format=json`;
+        const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityValue}&format=json`;
+        try {
 
-        let response = await axios.get(usUrl)
-        console.log(response.data[0]);
-        this.setState({location: response.data[0]})
-        
+            let response = await axios.get(url);
+            this.setState({location: response.data[0]});
+        } catch (event) {
+            this.setState({error: true});
+            this.setState({setError: event.response.status});
+        }
     }
     
     handleChange = (event) => {
     this.setState({ cityValue: event.target.value })
     }
 
+    hideModal = () => {
+        this.setState({error: false});
+    }
+
     render() {
+        let lat = this.state.location && this.state.location.lat;
+        let lon = this.state.location && this.state.location.lon;
+
         return (
-            <Form>
-                <InputGroup style={{width: '250px'}}>
-                    <InputGroup.Text>&#128506;&#65039;</InputGroup.Text>
-                    <Form.Control onChange={this.handleChange}/>
-                    <InputGroup.Text>&#128506;&#65039;</InputGroup.Text>
-                </InputGroup>
+            <div>
+                <Form>
+                    <InputGroup id='formInput' style={{width: '250px'}}>
+                        <InputGroup.Text>&#128506;&#65039;</InputGroup.Text>
+                        <Form.Control onChange={this.handleChange} placeholder='Enter City Name'/>
+                        <InputGroup.Text>&#128506;&#65039;</InputGroup.Text>
+                    </InputGroup>
 
-                <Button onClick={this.handleClick}>Explore</Button>
+                    <Button onClick={this.handleClick}>Explore</Button>
 
-                <Form.Group>
-                    <Form.Label><h3>City Name:</h3></Form.Label>
-                    <Form.Text>{this.state.location && <h1>{this.state.location.display_name}</h1>}</Form.Text>
-                </Form.Group>
+                    <Form.Group>
+                        <Form.Label><h3>Name:</h3></Form.Label>
+                        <Form.Text>{this.state.location && <h2>{this.state.location.display_name}</h2>}</Form.Text>
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label><h3>Latitude:</h3></Form.Label>
-                    <Form.Text>{this.state.location && <h1>{this.state.location.lat}</h1>}</Form.Text>
-                </Form.Group>
+                    <Form.Group id="lat">
+                        <Form.Label><h3>Latitude:</h3></Form.Label>
+                        <Form.Text><h2>{lat}</h2></Form.Text>
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label><h3>Longitude:</h3></Form.Label>
-                    <Form.Text>{this.state.location && <h1>{this.state.location.lon}</h1>}</Form.Text>
-                </Form.Group>
-            </Form>
+                    <Form.Group>
+                        <Form.Label><h3>Longitude:</h3></Form.Label>
+                        <Form.Text><h2>{lon}</h2></Form.Text>
+                    </Form.Group>
+                    <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${lat},${lon}&size=400x400&zoom=16`} alt={this.state.cityValue} />
+                </Form>
+                <Error setError={this.state.setError} show={this.state.error} hideModal={this.hideModal}/>
+            </div>
         )
     }
 }
